066b904abf767470a5f309122ab39439ed2a60a8
Lester Dela Cruz <lgdelacruz92@gmail.com>
2021-06-24 08:30:09 -0400

    added config validator

---
+++ src/player/bird.py
@@ -0,0 +1,10 @@
+class Bird:
+    def __init__(self, x: int, y: int, config=None):
+        if not x or not y:
+            raise Exception('x and y parameters are required')
+        self.x = x
+        self.y = y
+        self.v = 0
+        if config
+        self.g = 
+    
