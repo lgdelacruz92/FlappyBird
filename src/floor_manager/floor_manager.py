cbc9b5e4e999546c2fa6e58878b7eb2f8d914219
Lester Dela Cruz <lgdelacruz92@gmail.com>
2021-06-28 08:47:15 -0400

    Floor updates are working

--- src/floor_manager/floor_manager.py
+++ src/floor_manager/floor_manager.py
@@ -4,10 +4,11 @@ class FloorManager:
         if len(floors) < 2:
             raise ValueError('The number floors must be at least 2')
         self.floors = floors
+        first_x = floors[0].get_rect()[0]
         for i, floor in enumerate(self.floors):
             orig_rect = floor.get_rect()
             new_floor_y = self.game.screen.get_height() - orig_rect[3]
-            new_rect = (i * orig_rect[2], new_floor_y, orig_rect[2], orig_rect[3])
+            new_rect = (i * orig_rect[2] + first_x, new_floor_y, orig_rect[2], orig_rect[3])
             floor.set_rect(new_rect)
 
     def update(self):
@@ -20,3 +21,15 @@ class FloorManager:
             new_floor_x = orig_rect[0] - (self.game.config.floor_rate * dt)
             new_rect = (new_floor_x, orig_rect[1], orig_rect[2], orig_rect[3])
             floor.set_rect(new_rect)
+
+        # If first floor is no longer on screen
+        if self.floors[1].x < 0:
+            #wrap around
+            temp = self.floors[0]
+            for i in range(1, len(self.floors)):
+                self.floors[i-1] = self.floors[i]
+            self.floors[len(self.floors)-1] = temp
+            orig_rect = temp.get_rect()
+            new_rect = (orig_rect[0] + len(self.floors) * orig_rect[2], orig_rect[1], orig_rect[2], orig_rect[3])
+            self.floors[len(self.floors)-1].set_rect(new_rect)
+
--- tests/test_floor_manager.py
+++ tests/test_floor_manager.py
@@ -51,3 +51,33 @@ class TestFloorManager(unittest.TestCase):
         self.assertEqual(floor_manager.floors[0].x, -floor_rate * dt)
         self.assertEqual(floor_manager.floors[1].x, -floor_rate * dt + FLOOR_WIDTH)
         self.assertEqual(floor_manager.floors[2].x, -floor_rate * dt + FLOOR_WIDTH * 2)
+
+    def test_floor_wraps_around(self):
+        '''
+        Make sure the floors wraps around
+        '''
+
+        SCREEN_HEIGHT = 400
+        game = Mock()
+        game.screen.get_height.return_value = SCREEN_HEIGHT
+
+        # set floor rate
+        floor_rate = 2
+        game.config.floor_rate = floor_rate
+
+        # set delta time
+        dt = 2
+        game.clock.get_time.return_value = dt
+
+        FLOOR_WIDTH = 200
+        floors = []
+        for _ in range(3):
+            floors.append(Floor(-FLOOR_WIDTH, 0, FLOOR_WIDTH, 50, game))
+
+        # floor manager
+        floor_manager = FloorManager(floors, game)
+        floor_manager.update()
+
+        self.assertEqual(floor_manager.floors[0].x, -floor_rate * dt)
+        self.assertEqual(floor_manager.floors[1].x, -floor_rate * dt + FLOOR_WIDTH)
+        self.assertEqual(floor_manager.floors[2].x, -floor_rate * dt + FLOOR_WIDTH * 2)
