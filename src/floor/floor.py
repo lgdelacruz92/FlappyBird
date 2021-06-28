08d4eb57f52034d98cc86561af47a30f9cf92e65
Lester Dela Cruz <lgdelacruz92@gmail.com>
2021-06-28 06:03:46 -0400

    Floor is complete

--- src/floor/floor.py
+++ src/floor/floor.py
@@ -6,3 +6,11 @@ class Floor:
         self.w = w
         self.h = h
         self.y = self.game.screen.get_height() - h
+
+    def update(self):
+        dt = self.game.clock.get_time()
+        floor_rate = self.game.config.floor_rate
+        self.x -= dt * floor_rate
+
+    def get_rect(self):
+        return (self.x, self.y, self.w, self.h)
--- tests/test_floor.py
+++ tests/test_floor.py
@@ -26,3 +26,49 @@ class TestFloor(unittest.TestCase):
         floor = Floor(0,0,200, floor_height, self.game)
         self.assertEqual(floor.y, SCREEN_HEIGHT - floor_height)
 
+    def test_floor_update(self):
+        '''
+        Test that floor moves to the left according to game floor rate
+        '''
+        # initalize screen
+        SCREEN_WIDTH = 400
+        SCREEN_HEIGHT = 400
+        screen = Mock()
+        screen.get_width.return_value = SCREEN_WIDTH
+        screen.get_height.return_value = SCREEN_HEIGHT
+
+        # config
+        floor_rate_num = 5
+        config = Mock()
+        config.floor_rate = floor_rate_num
+
+        # pygame
+        dt = 2
+        clock = Mock()
+        clock.get_time.return_value = dt
+
+        # initialize game
+        self.game = Game(screen=screen, config=config, clock=clock)
+
+        floor = Floor(0, 0, 200, 50, self.game)
+        floor.update()
+        self.assertEqual(floor.x, -floor_rate_num * dt)
+        floor.update()
+        self.assertEqual(floor.x, -floor_rate_num * 2 * dt)
+
+    def test_get_rect(self):
+        '''
+        Test that get_rect returns proper values
+        '''
+        #initialize screen
+        SCREEN_HEIGHT = 400
+        screen = Mock()
+        screen.get_height.return_value = SCREEN_HEIGHT
+
+        # initialize game
+        self.game = Game(screen=screen)
+
+        floor = Floor(0, 0, 200, 50, self.game)
+        rect = floor.get_rect()
+        expected_rect = (0, 350, 200, 50)
+        self.assertEqual(rect, expected_rect)
